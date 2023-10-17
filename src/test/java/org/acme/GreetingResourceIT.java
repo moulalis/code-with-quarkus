ccc3a1a96d5b16d499eae6e17122480c85bf1c1d
Moulali Shikalwadi <59642466+moulalis@users.noreply.github.com>
2023-10-17 08:28:30 +0530

    Update GreetingResourceIT.java
    print added

--- src/test/java/org/acme/GreetingResourceIT.java
+++ src/test/java/org/acme/GreetingResourceIT.java
@@ -5,4 +5,5 @@ import io.quarkus.test.junit.QuarkusIntegrationTest;
 @QuarkusIntegrationTest
 public class GreetingResourceIT extends GreetingResourceTest {
     // Execute the same tests but in packaged mode.
+    System.out.println("Hello");
 }
